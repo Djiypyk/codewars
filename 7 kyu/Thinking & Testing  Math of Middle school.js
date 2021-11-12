45579eab0113c2a043fbee7c9e29a64f7ee50795
Mikalai Zarazaka <golmtz@gmail.com>
2021-11-12 15:49:55 +0300

    add kata

---
+++ 7 kyu/Thinking & Testing  Math of Middle school.js
@@ -0,0 +1,62 @@
+// Thinking & Testing : Math of Middle school
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at result of testcase, guess the code!
+
+// #Series:
+// 01:A and B?
+// 02:Incomplete string
+// 03:True or False
+// 04:Something capitalized
+// 05:Uniq or not Uniq
+// 06:Spatiotemporal index
+// 07:Math of Primary School
+// 08:Math of Middle school
+// 09:From nothingness To nothingness
+// 10:Not perfect? Throw away!
+// 11:Welcome to take the bus
+// 12:A happy day will come
+// 13:Sum of 15(Hetu Luosliu)
+// 14:Nebula or Vortex
+// 15:Sport Star
+// 16:Falsetto Rap Concert
+// 17:Wind whispers
+// 18:Mobile phone simulator
+// 19:Join but not join
+// 20:I hate big and small
+// 21:I want to become diabetic ;-)
+// 22:How many blocks?
+// 23:Operator hidden in a string
+// 24:Substring Magic
+// 25:Report about something
+// 26:Retention and discard I
+// 27:Retention and discard II
+// 28:How many "word"?
+// 29:Hail and Waterfall
+// 30:Love Forever
+// 31:Digital swimming pool
+// 32:Archery contest
+// 33:The repair of parchment
+// 34:Who are you?
+// 35:Safe position
+
+function testit(a, b) {
+  return [
+    a[0] * b[0] + a[1] * b[2],
+    a[0] * b[1] + a[1] * b[3],
+    a[2] * b[0] + a[3] * b[2],
+    a[2] * b[1] + a[3] * b[3],
+  ];
+}
+
+//Tips:Hard Version of Thinking & Testing 07
+// Test.assertSimilar(testit([0,0,0,0],[0,0,0,0]), [0,0,0,0], "")
+// Test.assertSimilar(testit([22,88,0,0],[0,100,0,100]), [0,11000,0,0], "")
+// Test.assertSimilar(testit([0,0,96,49],[86,0,53,0]), [0,0,10853,0], "")
+// Test.assertSimilar(testit([0,0,83,88],[0,27,0,99]), [0,0,0,10953], "")
+//Click "Submit" try more testcase...
