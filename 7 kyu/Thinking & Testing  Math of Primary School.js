47d522b8459fce2dc6d021c6fff7a8f1e3958f35
Mikalai Zarazaka <golmtz@gmail.com>
2021-11-12 15:54:14 +0300

    comlete kata

---
+++ 7 kyu/Thinking & Testing  Math of Primary School.js
@@ -0,0 +1,22 @@
+// Thinking & Testing : Math of Primary School
+
+// No Story
+
+// No Description
+
+// Only by Thinking and Testing
+
+// Look at the results of the testcases, and guess the code!
+
+function testit(arr) {
+  return arr[0] * arr[3] + arr[1] * arr[2];
+}
+
+//Add? subtract? multiply? divide? Who and who?
+// Test.assertSimilar(testit([0,0,0,0]), 0, "")
+// Test.assertSimilar(testit([0,0,0,1]), 0, "")
+// Test.assertSimilar(testit([0,0,1,1]), 0, "")
+// Test.assertSimilar(testit([0,1,0,1]), 0, "")
+// Test.assertSimilar(testit([0,1,1,0]), 1, "")
+// Test.assertSimilar(testit([1,0,0,1]), 1, "")
+//Click "Submit" try more testcase...
