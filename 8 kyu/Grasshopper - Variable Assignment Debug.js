a8f14806c02efcc0eef534f6c0761c60914859c2
Mikalai Zarazaka <golmtz@gmail.com>
2021-11-22 10:54:51 +0300

    Kata: Grasshopper - Variable Assignment Debug

---
+++ 8 kyu/Grasshopper - Variable Assignment Debug.js
@@ -0,0 +1,19 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+let a = "dev";
+let b = "Lab";
+
+let name = a + b;
+
+// describe('variable assignment', () => {
+//     it('should set variable a', () => {
+//       Test.assertEquals(a, 'dev')
+//     })
+//     it('should set variable b', () => {
+//       Test.assertEquals(b, 'Lab')
+//     })
+//     it('should set variable name', () => {
+//       Test.assertEquals(name, 'devLab')
+//     })
+//   })
